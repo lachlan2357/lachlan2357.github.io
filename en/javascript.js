bc26e3e4be9bbc9cb6e189ad8073e3e80abf0ff5
lachlan2357 <lachlan2357@outlook.com>
2021-06-08 21:57:47 +1000

    Betterer Scrollbar

--- en/javascript.js
+++ en/javascript.js
@@ -4,11 +4,14 @@ function testfunction() {
 
 	var percent = viewportHeight / bodyHeight;
 
-	document.getElementById("scrollbar").style.height = "calc(100vh * " + String(percent) + ")";
+	document.getElementById("scrollbar").style.height = "calc((100vh - 40px) * " + String(percent) + ")";
 
 	var scrolledHeight = window.pageYOffset;
 
 	var scrolledPercent = scrolledHeight / bodyHeight;
 
-	document.getElementById("scrollbar").style.top = "calc(100vh * " + String(scrolledPercent) + ")";
+	console.log(String(scrolledPercent));
+
+	document.getElementById("scrollbar").style.top = "calc(((100vh) * " + String(scrolledPercent) + ") + 10px)";
+	document.getElementById("scrollbar").style.opacity = "1";
 }
