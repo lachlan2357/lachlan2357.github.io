87da1477aaa4f0d18b4e12e98d71d2da18df692e
Lachlan Rehder <lachlan2357@outlook.com>
2023-06-19 20:32:02 +1000

    Incorporated Projects into Timeline properly

--- src/assets/projects.ts
+++ src/assets/projects.ts
@@ -12,6 +12,8 @@ const projects: Array<Project> = [
         title: "AMSI ChooseMaths",
         description:
             "Throughout 2017, 2018 and 2019, myself and a group of friends competed in the AMSI ChooseMATHS Competition. While not doing very well early on, our final video submission in 2019 was selected as one of the Top 10 nationally.",
+        month: 10,
+        year: 2019,
         icon: "fa-solid fa-film",
         gradOne: "#171c47",
         gradTwo: "#8b2370",
@@ -29,21 +31,22 @@ const projects: Array<Project> = [
         title: "F1 in Schools",
         description:
             "Myself and three friends competed in the 2020 F1 in Schools competition as Vitesse Racing, reaching third place at the State Competition. I was the graphic designer for our team, creating a team brand and website.",
+        month: 10,
+        year: 2020,
         icon: "fa-solid fa-car",
         gradOne: "#f22e0c",
         gradTwo: "#db0101",
         component: F1InSchools,
         roles: ["Graphic Designer"],
-        links: [
-            { name: "F1 in Schools Australia", url: "https://rea.org.au/f1-in-schools/" },
-            { name: "Vitesse Racing Website", url: "https://vitesseracing1.wixsite.com/website" }
-        ]
+        links: [{ name: "F1 in Schools Australia", url: "https://rea.org.au/f1-in-schools/" }]
     },
     {
         id: "this-website",
         title: "This very Website",
         description:
             "As a Quarantine Project spanning 2021, I set out to design and code a personal website, employing my skills in HTML, CSS and Javascript. This code behind the website was constructed with code written purely by me, with the source available via my Github Page.",
+        month: 6,
+        year: 2021,
         icon: "fa-solid fa-code",
         gradOne: "#6078e6",
         gradTwo: "#69b6fa",
@@ -58,6 +61,8 @@ const projects: Array<Project> = [
         description:
             "After the conclusion of Reddit's 2022 r/place event, I recreated Australia's pieces of art from our archived screenshots and turned them into displays.",
         icon: "fa-brands fa-reddit-alien",
+        month: 4,
+        year: 2022,
         gradOne: "#ff6e00",
         gradTwo: "#ff4500",
         component: AusRPlace,
@@ -73,6 +78,8 @@ const projects: Array<Project> = [
         description:
             "During my university studies, I began working on small project which required the use of colouring Python terminal outputs. As I felt existing solutions didn't offer the flexibility I was looking for, I developed my own.",
         icon: "fa-solid fa-brush",
+        month: 8,
+        year: 2022,
         gradOne: "#e01db6",
         gradTwo: "#e01d5a",
         roles: ["Creator", "Maintainer"],
@@ -87,6 +94,8 @@ const projects: Array<Project> = [
         description:
             "It was difficult to find a tool to help with visualising road information for data on OpenStreetMap due to the tiles used conveying all the data about a specific road. In response, I created Merge, a work-in-process but functional tool to visualise roads in more detail",
         icon: "fa-solid fa-map-location-dot",
+        month: 9,
+        year: 2022,
         gradOne: "#0bd674",
         gradTwo: "#0cdce0",
         component: MergeOSM,
@@ -102,12 +111,14 @@ const projects: Array<Project> = [
         description:
             "Whilst looking for a solution to send a list of Steam Games together, I decided to make my own. This would allow anybody to create a list of games and share a link to that collection.",
         icon: "fa-solid fa-gamepad",
+        month: 5,
+        year: 2023,
         gradOne: "#3dbdc6",
         gradTwo: "#0f9ca5",
         component: GameCollections,
         roles: ["Creator", "Maintainer"],
         links: [
-            { name: "Website", url: "https://lachlan2357.github.io/game-collections" },
+            { name: "Web Application", url: "https://lachlan2357.github.io/game-collections" },
             { name: "GitHub Page", url: "https://github.com/lachlan2357/game-collections" }
         ]
     },
@@ -116,6 +127,8 @@ const projects: Array<Project> = [
         title: "Lazy Image Generator",
         description:
             "To assist in creating lazy-loading images using a smaller resolution version as a placeholder, I created a small NodeJS tool to automatically downscale a directory of images, ensuring they were always up-to-date.",
+        month: 6,
+        year: 2023,
         icon: "fa-solid fa-clone",
         gradOne: "#6bb852",
         gradTwo: "#3e853f",
--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -32,22 +32,19 @@ export interface ChipData {
     url?: string;
 }
 
-export interface Project {
-    id: string;
-    title: string;
-    description: string;
-    icon: string;
-    gradOne: string;
-    gradTwo: string;
-    component?: Component;
-    roles: Array<string>;
-    links: Array<DisplayLink>;
-}
-
 export interface TimelineEvent {
     title: string;
     description: string;
     month: number;
     year: number;
     links: Array<DisplayLink>;
+}
+
+export interface Project extends TimelineEvent {
+    id: string;
+    icon: string;
+    gradOne: string;
+    gradTwo: string;
+    component?: Component;
+    roles: Array<string>;
 }
