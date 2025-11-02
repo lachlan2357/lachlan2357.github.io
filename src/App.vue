a9e8a6a6885d0201d402c0e74022540afba0e081
Lachlan Rehder <lachlan.rehder@outlook.com>
2025-11-03 00:12:59 +1000

    Implemented GitHub Pages 404 hack so SPAs work

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,30 @@
 <script setup lang="ts">
-    import { RouterView } from "vue-router";
+    import { RouterView, useRoute, useRouter } from "vue-router";
     import NavBar from "@/components/NavBar.vue";
     import PageHeader from "@/components/PageHeader.vue";
     import PageFooter from "./components/PageFooter.vue";
     import { useScrollPos } from "./stores/scroll";
+    import { onMounted, watch, watchEffect } from "vue";
+
+    const route = useRoute();
+    const router = useRouter();
 
     const scrollPos = useScrollPos();
 
     window.addEventListener("scroll", () =>
         scrollPos.setTop(document.documentElement.scrollTop ?? 0)
     );
+
+    // handle redirects
+    watchEffect(() => {
+        // get query param
+        const query = route.query;
+        const redirect = query.redirect;
+        if (!redirect) return;
+
+        // push to router
+        router.push(redirect.toString());
+    });
 </script>
 
 <template>
